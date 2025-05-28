fad141a53b43f86c6a7e09d213ec41ac818d5bff
otskey <scgodfrey@hotmail.com>
2025-05-28 16:15:54 +0100

    Updated lookbook page and styling. Added images.

--- src/app/lookbook/page.jsx
+++ src/app/lookbook/page.jsx
@@ -12,89 +12,90 @@ const gallery = [
   {
     id: 1,
     title: 'Maison Metapack | Spring Drop',
-    href: '#',
     date: 'April 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-01.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    image: {
+      src: "/images/lookbook1.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
   {
     id: 2,
     title: 'Maison Metapack | Spring Drop',
-    href: '#',
-    date: 'May 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-02.jpg',
-    imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
+    date: 'April 2023',
+    image: {
+      src: "/images/lookbook2.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
   {
     id: 3,
-    title: 'Maison Metapack | Christmas drop',
-    href: '#',
-    date: 'December 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-03.jpg',
-    imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
+    title: 'Maison Metapack | Spring Drop',
+    date: 'April 2023',
+    image: {
+      src: "/images/lookbook3.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
  {
     id: 4,
     title: 'Maison Metapack | Spring Drop',
-    href: '#',
     date: 'April 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-01.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    image: {
+      src: "/images/lookbook4.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
   {
     id: 5,
     title: 'Maison Metapack | Spring Drop',
-    href: '#',
-    date: 'May 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-02.jpg',
-    imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
+    date: 'April 2023',
+    image: {
+      src: "/images/lookbook5.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
   {
     id: 6,
-    title: 'Maison Metapack | Christmas drop',
-    href: '#',
-    date: 'December 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-03.jpg',
-    imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
+    title: 'Maison Metapack | Spring Drop',
+    date: 'April 2023',
+    image: {
+      src: "/images/lookbook6.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
    {
     id: 7,
     title: 'Maison Metapack | Spring Drop',
-    href: '#',
     date: 'April 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-01.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    image: {
+      src: "/images/lookbook7.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
   {
     id: 8,
     title: 'Maison Metapack | Spring Drop',
-    href: '#',
-    date: 'May 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-02.jpg',
-    imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
+    date: 'April 2023',
+    image: {
+      src: "/images/lookbook8.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
   {
     id: 9,
-    title: 'Maison Metapack | Christmas drop',
-    href: '#',
-    date: 'December 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-03.jpg',
-    imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
-  },
-  {
-    id: 10,
-    title: 'Maison Metapack | Christmas drop',
-    href: '#',
-    date: 'December 2023',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-01-image-card-03.jpg',
-    imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
+    title: 'Maison Metapack | Spring Drop',
+    date: 'April 2023',
+    image: {
+      src: "/images/lookbook9.png",
+      alt: "Hipster delivery driver wearing a Metapack t-shirt and cap, holding a Metapack package.",
+    },
   },
 ];
 
 export default function LookBookPage() {
   return (
     <>
-      <LookBook gallery={gallery} />
+      <LookBook items={gallery} />
     </>
   );
 }
--- src/components/LookBook.jsx
+++ src/components/LookBook.jsx
@@ -1,30 +1,32 @@
-"use client"
-import Image from "next/image"
+  "use client"
+  import Image from "next/image"
 
-export default function LookBook({ gallery }) {
+  export default function LookBook({ items }) {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 id="gallery-heading" className="sr-only">
-          Lookbook
-        </h2>
+        <h2 id="gallery-heading" className="sr-only">Lookbook</h2>
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-          {gallery.map((gallery) => (
-            <a key={gallery.id} href={gallery.href} className="group">
-              <img
-                alt={gallery.imageAlt}
-                src={gallery.imageSrc}
-                className="aspect-square w-full overflow-hidden rounded-lg object-cover group-hover:opacity-75 sm:aspect-2/3"
-              />
-              <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                <h3>{gallery.title}</h3>
+          {items.map((item) => (
+            <a key={item.id} href={item.href} className="group">
+              <div className="relative w-full h-140">
+                <Image
+                  src={item.image.src}
+                  alt={item.image.alt}
+                  fill
+                  className="object-cover group-hover:opacity-75"
+                  sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                />
               </div>
-              <p className="mt-1 text-sm text-gray-500 italic">{gallery.date}</p>
+              <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
+                <h3>{item.title}</h3>
+              </div>
+              <p className="mt-1 text-sm text-gray-500 italic">{item.date}</p>
             </a>
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
